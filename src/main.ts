ee878cc76fb8524f8b8c0c198604293076bfd111
Bogdan <bogdasafronov@gmail.com>
2024-03-02 19:01:33 +0300

    chore: add class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      whitelist: true,
+    }),
+  );
+
+  await app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
 }
 bootstrap();
